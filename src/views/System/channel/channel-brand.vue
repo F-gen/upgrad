bbb125928703e40b40192fb0189b8557f2dc65f4
F-gen <57865916+F-gen@users.noreply.github.com>
2022-08-07 17:22:13 +0800

    Update channel-brand.vue

--- src/views/System/channel/channel-brand.vue
+++ src/views/System/channel/channel-brand.vue
@@ -1,25 +1,143 @@
-<!--
- * @Author: eureka fugen1999@163.com
- * @Date: 2022-08-07 11:50:39
- * @LastEditors: eureka
- * @LastEditTime: 2022-08-07 11:54:05
- * @FilePath: \upgrad-main\src\views\System\channel\channel-brand.vue
- * @Description: 
- * 
- * Copyright (c) 2022 by eureka, All Rights Reserved. 
--->
+
 <template>
-  <div >
-   channel-brand
+  <div>
+    <a-form layout="inline">
+      <a-form-item class="w-[200px]">
+        <a-select
+          v-model:value="searchText"
+          show-search
+          :filter-option="false"
+          :show-arrow="false"
+          @search="SearchBrand"
+          @blur="getBrandAgain"
+        >
+          <a-select-option
+            v-for="(item, index) in BrandItems"
+            :key="index"
+            :value="item"
+          >
+            {{ item }}
+          </a-select-option>
+        </a-select>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" @click="onSearch">
+          <span
+            class="iconify inline align-middle mr-1 mb-1 text-sm"
+            data-icon="material-symbols:search"
+            data-inline="false"
+          />
+          <span class="inline">Search</span>
+        </a-button>
+        <a-button type="primary" class="ml-3" @click="reset">
+          <span
+            class="iconify inline align-middle mr-1 mb-1 text-sm"
+            data-icon="carbon:reset"
+            data-inline="false"
+          />
+          <span class="inline">Reset</span>
+        </a-button>
+      </a-form-item>
+    </a-form>
   </div>
+  <div class="mt-4">
+    <a-table
+      :data-source="tabData"
+      bordered
+      :pagination="paginationOption"
+      :columns="columns"
+      :scroll="{ y: 'calc(100vh - 376px)' }"
+    >
+      >
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'action'">
+          <a @click="edit(record)">Edit</a>
+        </template>
+        <template v-if="column.key === 'brandChannelInfo'">
+          <a-tag
+            v-for="item in record.brandChannelInfo"
+            :key="item.channelName"
+          >
+            {{ item.channelName }}
+          </a-tag>
+        </template>
+      </template>
+    </a-table>
+  </div>
+  <ChannelBrandEdit ref="editchannel" @refresh="onSearch" />
 </template>
 
 <script>
+import ChannelBrandEdit from "./channel-brand-edit.vue";
 export default {
-  name: 'channel-brand',
+  name: "channel-brand",
 };
 </script>
 
 <script setup>
+const paginationOption = reactive({
+  showQuickJumper: true,
+  pageSizeOptions: ["10", "20", "30", "40", "50", "100"],
+  showSizeChanger: true,
+  defaultPageSize: 50,
+});
+const columns = reactive([
+  { title: "NO", dataIndex: "key", key: "key" },
+  { title: "Brand Eng Name", dataIndex: "brandName", key: "brandName" },
+  { title: "Channel", dataIndex: "brandChannelInfo", key: "brandChannelInfo" },
+  { title: "Action", dataIndex: "action", key: "action" },
+]);
+let BrandItems = ref([]);
+let BackupBrandItems = ref([]);
+const editchannel = ref(null);
+let searchText = ref("");
+const SearchBrand = (value) => {
+  let timer;
+  if (timer) clearTimeout(this.timer);
+  timer = setTimeout(async () => {
+    BrandItems.value = await api.queryAllBrandName({ keyword: value });
+    clearTimeout(timer);
+  }, 1000);
+  searchText.value = value;
+};
+const getBrandAgain = () => {
+  BrandItems.value = BackupBrandItems.value;
+};
+let tabData = ref([]);
+const channelInfo = ref([]);
+const getBaseData = async () => {
+  const data = await api.queryAllBrandName();
+  BrandItems.value = data;
+  BackupBrandItems.value = data;
+  channelInfo.value = await api.queryChannel();
+};
+const onSearch = async () => {
+  let data = await api.queryBrandChannel({
+    keyword: searchText.value,
+  });
+  // this.tabData = data;
+  tabData.value = data.map((item, index) => {
+    item.key = index + 1;
+    return item;
+  });
+};
+const reset = () => {
+  searchText.value = "";
+  BrandItems.value = BackupBrandItems.value;
+  onSearch();
+};
+const edit = (record) => {
+  editchannel.value.show = true;
+  editchannel.value.item.brandId = record.brandId;
+  editchannel.value.channelDetail = channelInfo.value;
+  editchannel.value.item.brandName = record.brandName;
+  let obj = [];
+  record.brandChannelInfo.forEach((val) => {
+    obj.push(val.channelName);
+  });
 
+  editchannel.value.item.brandChannelInfo = obj;
+};
+getBaseData();
+onSearch();
 </script>
