cb6e4f9863453e49b23af130e47ab26650a88f17
F-gen <57865916+F-gen@users.noreply.github.com>
2022-08-07 14:16:55 +0800

    Update channel.vue

--- src/views/System/channel/channel.vue
+++ src/views/System/channel/channel.vue
@@ -1,17 +1,42 @@
+<!--
+ * @Author: eureka fugen1999@163.com
+ * @Date: 2022-08-06 10:07:50
+ * @LastEditors: eureka
+ * @LastEditTime: 2022-08-07 12:22:45
+ * @FilePath: \upgrad-main\src\views\System\channel\channel.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by eureka, All Rights Reserved. 
+-->
+
 <template>
-  <div>channle</div>.
-  <a-table>
-    123
-  </a-table>
+  <div class="bg-[#f0f2f5] w-full box-border p-6">
+    <div class="bg-[#fff] p-6">
+      <a-tabs v-model:activeKey="activeKey" @change="changtab">
+        <a-tab-pane key="channelBrand" tab="Brand Channel" />
+        <a-tab-pane key="channelConfig" tab="Channel" />
+      </a-tabs>
+      <component :is="componentList[componentId]" />
+    </div>
+  </div>
 </template>
 
 <script>
+import channelConfig from './channel-config.vue';
+import channelBrand from './channel-brand.vue';
 export default {
-  name: 'Channel',
-  setup() {
-    return {}
-  }
-}
+  name: "Keyword",
+};
+let componentList = {
+  channelConfig,
+  channelBrand,
+};
 </script>
-<style lang="less" scoped>
-</style>
+
+<script setup>
+const activeKey = ref("channelBrand");
+const componentId = ref("channelBrand");
+const changtab = (activeKey) => {
+  componentId.value = activeKey;
+};
+</script>
