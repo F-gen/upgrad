929af94f8cbe75290ff12d5af9bd97e0b5ba2cbe
F-gen <57865916+F-gen@users.noreply.github.com>
2022-08-08 17:23:15 +0800

    Update platform.vue

--- src/views/System/platform/platform.vue
+++ src/views/System/platform/platform.vue
@@ -1,14 +1,46 @@
+<!--
+ * @Author: eureka fugen1999@163.com
+ * @Date: 2022-08-06 10:07:50
+ * @LastEditors: eureka
+ * @LastEditTime: 2022-08-08 17:21:36
+ * @FilePath: \upgrad-main\src\views\System\platform\platform.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by eureka, All Rights Reserved. 
+-->
+
 <template>
-  <div>platform </div>
+  <div class="bg-[#f0f2f5] w-full box-border p-6">
+    <div class="bg-[#fff] p-6">
+      <a-tabs v-model:activeKey="activeKey" @change="changtab">
+        <a-tab-pane key="PlatformDetail" tab="Brand Platform" />
+        <a-tab-pane
+          key="PlatformConfig"
+          tab="Platform
+"
+        />
+      </a-tabs>
+      <component :is="componentList[componentId]" />
+    </div>
+  </div>
 </template>
 
 <script>
+import PlatformConfig from "./platform-config.vue";
+import PlatformDetail from "./platform-brand.vue";
 export default {
-  name: 'Platform',
-  setup() {
-    return {}
-  }
-}
+  name: "platform",
+};
+let componentList = {
+  PlatformConfig,
+  PlatformDetail,
+};
 </script>
-<style lang="less" scoped>
-</style>
+
+<script setup>
+const activeKey = ref("PlatformDetail");
+const componentId = ref("PlatformDetail");
+const changtab = (activeKey) => {
+  componentId.value = activeKey;
+};
+</script>
