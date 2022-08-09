c40fcdac11fe645a44ce86f0f9e0a3810fff2765
F-gen <57865916+F-gen@users.noreply.github.com>
2022-08-09 15:03:01 +0800

    Update login.vue

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -15,8 +15,8 @@ const loginFn = async () => {
   const store = useStore();
   const router = useRouter();
   if (window.location.hostname === "localhost") {
-    // token = await api.getFutailToken();
-    token = "0c6fdc60-0730-40bf-9b05-f8f75bf27866"
+     token = await api.getFutailToken();
+   // token = "0c6fdc60-0730-40bf-9b05-f8f75bf27866"
 
   } else {
     token = router.query.token;
