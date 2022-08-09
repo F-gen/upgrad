a4bb79de151da37611a2886b25fbe0a974358e9e
F-gen <57865916+F-gen@users.noreply.github.com>
2022-08-09 18:34:26 +0800

    Update topic.vue

--- src/views/System/TopicTag/topic.vue
+++ src/views/System/TopicTag/topic.vue
@@ -1,16 +1,41 @@
+<!--
+ * @Author: eureka fugen1999@163.com
+ * @Date: 2022-08-06 10:07:50
+ * @LastEditors: eureka
+ * @LastEditTime: 2022-08-09 18:26:02
+ * @FilePath: \upgrad-main\src\views\System\TopicTag\topic.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by eureka, All Rights Reserved. 
+-->
 <template>
-  <div>
-    <h1>topic.vue</h1>
+  <div class="bg-[#f0f2f5] w-full box-border p-6">
+    <div class="bg-[#fff] p-6">
+      <a-tabs v-model:activeKey="activeKey" @change="changtab">
+        <a-tab-pane key="TagConfig" tab="Tag" />
+        <a-tab-pane key="TopicConfig" tab="Topic" />
+      </a-tabs>
+      <component :is="componentList[componentId]" />
+    </div>
   </div>
 </template>
 
 <script>
+import TopicConfig from "./topic-config.vue";
+import TagConfig from "./tag-config.vue";
 export default {
-  name: 'Topic',
-  setup() {
-    return {}
-  }
-}
+  name: "topictag",
+};
+let componentList = {
+  TagConfig,
+  TopicConfig,
+};
 </script>
-<style lang="less" scoped>
-</style>
+
+<script setup>
+const activeKey = ref("TagConfig");
+const componentId = ref("TagConfig");
+const changtab = (activeKey) => {
+  componentId.value = activeKey;
+};
+</script>
