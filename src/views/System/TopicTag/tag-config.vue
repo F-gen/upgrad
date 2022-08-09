1c72cc18b07759d648aa0495427e68a73949cafc
F-gen <57865916+F-gen@users.noreply.github.com>
2022-08-09 18:56:07 +0800

    Add files via upload

---
+++ src/views/System/TopicTag/tag-config.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: eureka fugen1999@163.com
+ * @Date: 2022-08-09 17:12:25
+ * @LastEditors: eureka
+ * @LastEditTime: 2022-08-09 17:15:32
+ * @FilePath: \upgrad-main\src\views\System\TopicTag\tag-config.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by eureka, All Rights Reserved. 
+-->
+<template>
+  <div >
+   tag
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tag',
+};
+</script>
+
+<script setup>
+
+</script>
